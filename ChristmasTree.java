c0fd869bf6e43180c75b836b46b1cce52a29aa0d
Isabe Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2018-01-06 18:29:33 -0500

    Putting calculations on how the stars are created

--- ChristmasTree.java
+++ ChristmasTree.java
@@ -17,23 +17,23 @@ public class ChristmasTree {
       //Step2: Pick up the pen
     Graphics c= canvas.getGraphics();
   // Method trying
-    c.setColor(new Color(0, 102, 0));
-    Triangles(220,250,280,200,120,200,c);
     Trunk(200,300,400,440,c);
-    SmallStar(92,101,105,109,118,178,183,195,200,c);
-    SmallStar(389,398,402,406,415,127,132,144,149,c);
-    SmallStar(160,169,173,177,186,50,55,67,72,c);
-    SmallStar(360,369,373,377,386,25,30,42,47,c);
-    SmallStar(27,36,40,44,53,70,75,87,92,c);
-    SmallStar(37,46,50,54,63,267,272,284,289,c);
-    SmallStar(363,372,376,380,389,210,215,227,232,c);
-    SmallStar(430,439,443,447,456,272,277,289,293,c);
-    SmallStar(300,309,313,317,326,80,85,97,103,c);
+    Triangles(220,250,280,200,120,200,c);
+    Star(92,178,c);
+    Star(389,127,c);
+    Star(160,50,c);
+    Star(360,25,c);
+    Star(27,70,c);
+    Star(37,267,c);
+    Star(363,210,c);
+    Star(430,272,c);
+    Star(300,80,c);
     canvas.setBackground(new Color(0,0,0));
     Lights(c, canvas);
 
   }
   public static void Triangles(int x1,int x2,int x3, int y1, int y2, int y3, Graphics c){
+    c.setColor(new Color(0, 102, 0));
     int xpoints []= {x1,x2,x3};
     int ypoints []= {y1,y2,y3};
     for (int i=1; i<=8; i++){
@@ -119,19 +119,31 @@ public class ChristmasTree {
     PositionLights(244,206,width,height,c);
 }
   public static void NotOrange(int width2, int height2,Graphics c){
-    c.setColor(new Color(0, 102, 0));
+    c.setColor(new Color(0,102, 0));
     PositionLights(287,299,width2,height2,c);
     PositionLights(182,313,width2,height2,c);
     PositionLights(241,205,width2,height2,c);
 
 }
-  public static void SmallStar(int x1, int x2, int x3, int x4, int x5, int y1, int y2, int y3, int y4,Graphics c){
+  public static void StarDrawing(int x1, int x2, int x3, int x4, int x5, int y1, int y2, int y3, int y4,Graphics c){
     c.setColor(new Color(255, 207, 0));
     int xpoints[]= {x1,x3,x5,x1,x2,x1,x3,x5,x4};
     int ypoints[]={y3,y1,y3,y3,y2,y2,y4,y2,y2};
     c.drawPolygon(xpoints, ypoints, 9);
     c.fillPolygon(xpoints,ypoints,9);
 
+  }
+  public static void Star (int x, int y,Graphics c){
+    int x1= x;
+    int x2= x1+9;
+    int x3= x2+4;
+    int x4= x3+4;
+    int x5= x4+9;
+    int y1= y;
+    int y2= y1+5;
+    int y3= y2+12;
+    int y4= y3+5;
+    StarDrawing(x1,x2,x3,x4,x5,y1,y2,y3,y4,c);
   }
   //This is for music ~ figure out by watching the YouTube Video
   /*public static class AL implements ActionListener{
